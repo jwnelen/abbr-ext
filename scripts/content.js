ebca646888204c04f86aa0731234c10d5680ca25
Jeroen Nelen <jwnelen@gmail.com>
2024-07-15 15:50:14 +0200

    small improvements

--- scripts/content.js
+++ scripts/content.js
@@ -8,18 +8,13 @@ map.set("Kralingse plas", "Krapla");
 map.set("kralingse plas", "Krapla");
 map.set("aanbieding", "in de bonus");
 
-// list of map keys
-// var words = Object.keys(map);
 var words = Array.from(map.keys());
 var regex = new RegExp("(" + words.join("|") + ")", "g");
-console.log(regex);
 
 var replacer = function (value) {
   var res = map.get(value);
   return res;
 };
-
-// /(vaatwasser|Vaatwasser|Kralingse Plas)/g
 
 // Getting part of the page text
 function replaceRecursively(element) {
@@ -34,4 +29,3 @@ function replaceRecursively(element) {
 }
 
 replaceRecursively(document.body);
-abbr-ext
